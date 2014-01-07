2c5bb5f3e59a30bc75cb5c97801e6263d4a1b93f
mstknk <mesutkonak89@gmail.com>
2014-01-08 00:24:12 +0100

    asdasd
    asdsadasdsad

---
+++ project/src/test/java/project/Test.java
@@ -0,0 +1,18 @@
+package project;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		int x=2;
+		assertEquals(2, x);
+	}
+	@org.junit.Test
+	public void test2(){
+		boolean x=true;
+		assertTrue(x);
+	}
+
+}
